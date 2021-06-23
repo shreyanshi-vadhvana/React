7a85752a94307527606ae7f0dbbe710dd56d1021
shreyanshi-vadhvana <shreyanshi.dv3@gmail.com>
2021-06-23 23:57:55 +0530

    React Router

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -2,16 +2,16 @@ import React from 'react';
 
 function Footer(props) {
     return(
-    <div className="footer">
+    <div className="footer">s
         <div className="container">
             <div className="row justify-content-center">             
                 <div className="col-4 offset-1 col-sm-2">
                     <h5>Links</h5>
                     <ul className="list-unstyled">
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">About</a></li>
-                        <li><a href="#">Menu</a></li>
-                        <li><a href="contactus.html">Contact</a></li>
+                        <li><a href="/home">Home</a></li>
+                        <li><a href="/aboutus">About</a></li>
+                        <li><a href="/menu">Menu</a></li>
+                        <li><a href="/contactus">Contact</a></li>
                     </ul>
                 </div>
                 <div className="col-7 col-sm-5">
